5008fe5ea135a2f8d419b63c1239f0145a8047ea
biyujiao <biyujiao.autohome.com.cn>
2020-09-11 18:48:50 +0800

    :

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 // import Person from './component/PropTypes';
 // import NewContext from './component/context/NewContext';
-import Pure from './component/pure/Pure';
+// import Pure from './component/pure/Pure';
+// import Logger from './component/high/Logger';
+import MouseTracker from './component/render-props/MouseTracker';
+import Picture from './component/render-props/Picture';
 
-let props = {
-    name: 'abc',
-    age: 18,
-    gender: 'man',
-    isMarried: true,
-    hobby: ['1', '2'],
-    position: {x:100,y:100}
-};
+ReactDOM.render(<MouseTracker render={
+    (props)=>{
+        return (
+            <Picture {...props}/>
+        )
+    }
+}/>, document.getElementById('root'));
 
-ReactDOM.render(<Pure/>, document.getElementById('root'));
+
+// ReactDOM.render(<Logger name="Logger名字"/>, document.getElementById('root'));
 
 // ReactDOM.render(<Form name='爸爸'/>, document.getElementById('root'));
