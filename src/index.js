1c1d7db318527fad5fa4e801de8e3542977f0e46
biyujiao <biyujiao.autohome.com.cn>
2020-09-14 18:52:00 +0800

    1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 // import Person from './component/PropTypes';
 // import NewContext from './component/context/NewContext';
 // import Pure from './component/pure/Pure';
 // import Logger from './component/high/Logger';
-import MouseTracker from './component/render-props/MouseTracker';
-import Picture from './component/render-props/Picture';
+// import MouseTracker from './component/render-props/MouseTracker';
+// import Picture from './component/render-props/Picture';
+
+function Counter() {
+    let [number, setNumber]=useState(0);
+    return (
+        <>
+            <div>{number}</div>
+            <button onClick={()=>setNumber(number+1)} > + </button>
+        </>
+    );
+}
+
+ReactDOM.render(<Counter />, document.getElementById('root'));
+/* render-props
 
 ReactDOM.render(<MouseTracker render={
     (props)=>{
@@ -14,7 +27,7 @@ ReactDOM.render(<MouseTracker render={
         )
     }
 }/>, document.getElementById('root'));
-
+*/
 
 // ReactDOM.render(<Logger name="Logger名字"/>, document.getElementById('root'));
 
